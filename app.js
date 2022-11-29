76b555d7c70b3d80ea387faf4a8b267d3b4cf156
Roman <mazaxaka.tyt@gmail.com>
2022-11-29 21:17:17 +0200

    add slider in companies section

--- app.js
+++ app.js
@@ -52,3 +52,12 @@ $(document).ready(function () {
     variableWidth: true,
   });
 });
+
+$(document).ready(function () {
+  $(".companies-slider").slick({
+    arrows: false,
+    infinite: false,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
